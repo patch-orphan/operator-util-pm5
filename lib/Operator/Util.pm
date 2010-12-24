a1277f1e5a42077e06da85d4093d916b640fb1e4
Nick Patch <n@atemoya.net>
2010-12-23 19:51:41 -0500

    new project

---
+++ lib/Operator/Util.pm
@@ -0,0 +1,62 @@
+package Operator::Util;
+use 5.006;
+use strict;
+use warnings;
+use parent 'Exporter';
+
+our $VERSION     = '0.00_1';
+our @EXPORT_OK   = qw( );
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+1;
+
+__END__
+
+=head1 NAME
+
+Operator::Util - A selection of subroutines based on metaoperators in Perl 6
+
+=head1 VERSION
+
+This document describes Operator::Util version 0.00_1.
+
+=head1 SYNOPSIS
+
+    use Operator::Util;
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS
+
+The following functions are provided but are not exported by default.
+
+=over 4
+
+=item XXX
+
+...
+
+=back
+
+=head1 SEE ALSO
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
